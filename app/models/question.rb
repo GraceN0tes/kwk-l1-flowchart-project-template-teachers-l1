467058575be30dfe474de42f69bdbc660929de1b
Grace Park <parkgra3@gmail.com>
2018-08-29 15:20:31 +0000

    automatically backed up by learn

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,15 @@
+#this file should contain ruby and does not have to be a class - could be a method or other ruby
+# dont run filter_parameter_logging#gem install sinatra
+
+#no 
+#instead
+
+#bundle install sinatra
+#should only have to do bundle install once
+#use a google doc to keep track of what you write so others can have it under check too
+#basics - make page look great first and then add features
+
+
 class Question
 
 end
